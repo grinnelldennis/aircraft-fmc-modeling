959164ba8f22b6fad8d91fb7fbff5eb9f9043a88
grinnelldennis <chanchun@grinnell.edu>
2017-05-25 10:44:21 -0500

    initial update

---
+++ Aircraft.java
@@ -0,0 +1,15 @@
+// Import statements
+
+// Stores all static data for the aircraft
+// NO PERFORMANCE DATA
+class Aircraft {
+  
+  public Aircraft (String type) {
+    File file = new Flie (type);
+
+    // use hashmap to store all attributes
+    HashMap aircraft = new HashMap(); 
+
+
+  }
+}
---
+++ Performance.java
@@ -0,0 +1,9 @@
+
+class Performance {
+
+  public Performance () {
+
+    
+  }
+
+}
