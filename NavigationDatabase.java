1a7a2e670f287b2bb0ac582d8895e060f9ca11ca
grinnelldennis <chanchun@grinnell.edu>
2017-05-31 14:08:11 -0500

    base upload

--- NavigationDatabase.java
+++ NavigationDatabase.java
@@ -1,52 +1,13 @@
 
 // Read nav and airport informations into respective hashmaps within the program
 class NavigationDatabase {
-  final String path = "/data"; 
   // airports; key: icao, value: Airport object
   HashMap<String, Airport> airports;
   // navaids: key: designator(non-unique), value: ArrayList<Navaid object>
   HashMap<String, ArrayList<Navaid>> navaids;
 
   public NavigationDatabase () {
-    airports = new HashMap<>();
-    navaids = new HashMap<>();
-    buildDatabase();
-  }
-  public void buildDatabase() {
-    // TODO: fill path
-    readNavFromFile(new File(path+"/")); 
-    readAirportsFromFile(new File(path+"/"));
-  }
-
-
-  // Initialize Navaids from file
-  private void readNavFromFile(File f) {
-    Scanner scanf = new Scanner(f);
-    while (scanf.hasNext())
-      addNavaidToHash(scanf.nextLine());
-  }
-
-  private void addNavaidToHash(String s) {
-    // TODO: fill information delimiting pattern
-    String[] f = s.split("--STUB--");
-    String ident = f[-1];           
-    if (!navaids.contains(ident))
-      navaids.add(ident, new Arraylist<Navaid>());
-    navaids.set(ident, navaids.get(ident).add(new Navaid()));
-  }
-
-  // Initialize Airports from file
-  private void readAirportsFromFile(File f) {
-    Scanner scanf = new Scanner(f);
-    while (scanf.hasNext())
-      addAirportToHash(scanf.nextLine());
-  }
-
-  private void addAirportToHash(String s) {
-    // TODO: fill information delimiting pattern
-    String[] f = s.split("--STUB--");       
-    String icao = f[-1];            
-    airports.add(icao, new Airport());                  // create new entry for navaids
+    BuildDatabase build = new BuildDatabase();
   }
 
 
--- Performance.java
+++ Performance.java
@@ -1,9 +1,28 @@
 
 class Performance {
 
-  public Performance () {
+  Aircraft ac;
 
+  public Performance (Aircraft ac) {
+    this.ac = ac;
+  }
+
+  public getDistanceWithMinutes(double minutes) {
     
   }
+
+  public getTopOfClimb() {}
+
+  public getTopOfDescend(int top, int bottom, int glideSlope) { return (top - bottom) / glideSlope; }
+
+  public getRateOfDescend(int groundSpeed) { return groundSpeed * 5; };
+
+  public getRateOfDescend(int tas, int top, int bottom, int distance) { 
+    return (tas * (top - bottom))/ distance; 
+  }
+  
+  public getDescendPoint() {}
+
+  public getVref () {}
 
 }
